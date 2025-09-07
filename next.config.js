7c3462a228c9072c7ea588fe017b3ad007e22eed
Danylo Kachur <danylo2006@gmail.com>
2025-09-06 22:22:01 -0400

    clean up env typesafety

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-import "./src/env.js";
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import env here to validate during build. Using jiti we can import .ts files :)
+jiti("./src/env.ts");
 
 /** @type {import("next").NextConfig} */
 const config = {};
